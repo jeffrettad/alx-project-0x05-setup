7355f14cc285e591d8131aa5ce9103da4f05bcb8
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-12-07 23:22:13 +0100

    Update Layout.tsx

--- alx-project-0x07/components/layouts/Layout.tsx
+++ alx-project-0x07/components/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactComponentProps} from "@/interfaces";
+jimport { ReactComponentProps} from "@/interfaces";
 import Footer from "./Footer"
 import Header from "./Header"
 const Layout: React.FC<ReactComponentProps> = ({ children}) => {
