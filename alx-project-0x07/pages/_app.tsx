21c367dbcb89553055ff9091da50ae86d985773f
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-12-07 23:23:59 +0100

    Update _app.tsx

--- alx-project-0x07/pages/_app.tsx
+++ alx-project-0x07/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/layouts/Layout";
+himport Layout from "@/components/layouts/Layout";
 import type { AppProps } from "next/app";
 
 export default function App ({Component pageProps }: AppProps){
